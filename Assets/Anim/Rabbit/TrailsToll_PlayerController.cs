3e808cb6f07d605af63669da5f7d0c557d5b81d4
Kiran Jodhani <kiran.jodhani.w@gmail.com>
2020-05-06 14:26:01 +0530

    nn

---
+++ Assets/Anim/Rabbit/TrailsToll_PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailsToll_PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator PlayerAnimator;
+    void Start()
+    {
+        //PlayerAnimator.SetTrigger("Idle");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Z))
+        {
+            PlayerAnimator.SetTrigger("Idle");
+        }
+        else if (Input.GetKeyUp(KeyCode.X))
+        {
+            PlayerAnimator.SetTrigger("Run");
+        }
+        else if (Input.GetKeyUp(KeyCode.C))
+        {
+            PlayerAnimator.SetTrigger("Up");
+        }
+        else if (Input.GetKeyUp(KeyCode.V))
+        {
+            PlayerAnimator.SetTrigger("Roll");
+        }
+        else if (Input.GetKeyUp(KeyCode.B))
+        {
+            PlayerAnimator.SetTrigger("Failure");
+        }
+    }
+}
