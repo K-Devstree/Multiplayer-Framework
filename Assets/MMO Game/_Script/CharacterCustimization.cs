181531a490593f078b0b7a96b212c4d532eb202e
Kiran Jodhani <kiran.jodhani.w@gmail.com>
2020-08-11 19:09:31 +0530

    11 aug

---
+++ Assets/MMO Game/_Script/CharacterCustimization.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CharacterCustimization : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CharacterBaseClass CharacterBaseClassInstance;
+    public int SelectedCharacterModel;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowSelectedModel(int ModelIndex)
+    {
+        SelectedCharacterModel = ModelIndex;
+        for (int i = 0; i < 2;i++)
+        {
+            CharacterBaseClassInstance.CharacterMainInstance[i].Characters.SetActive(false);
+        }
+        CharacterBaseClassInstance.CharacterMainInstance[SelectedCharacterModel].Characters.SetActive(true);
+    }
+}
+
+
+[Serializable]
+public class CharacterBaseClass
+{
+    public CharacterMain[] CharacterMainInstance;
+}
+
+[Serializable]
+public class CharacterMain
+{
+    public GameObject Characters;
+    public Material HairMaterial;
+    public Material SkinMaterial;
+    public Material ShirtMaterial;
+    public Material PentMaterial;
+    public Material ShoesMaterial;
+}
