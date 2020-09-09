5b05055df5cc2ca1b48b3c2782b7fde9fad4bc61
Yash <yashs.devstree@gmail.com>
2020-09-09 15:46:08 +0530

    YASH_WIN_20200909
     - Removed FPS Package
     - Created custom camera and player controller

---
+++ Assets/MMO Game/_Script/PlayerCameraController.cs
@@ -0,0 +1,185 @@
+﻿using UnityEngine;
+
+public enum CameraPerspective
+{
+    ThirdPerson,
+    FirstPerson
+}
+
+public class PlayerCameraController : MonoBehaviour
+{
+    [Header("Common Camera Settings")]
+    public bool lockCursor = true;
+    public CameraPerspective cameraPerspective;
+    public float mouseSensitivity = 25f;
+
+    [Header("Character")]
+    public Transform CharacterControllerTransform;
+    public Animator CharacterAnimator;
+
+    [Header("Camera Settings")]
+    public Vector3 FPS_CameraOffset = new Vector3(0f, 0.1f, 0.25f);
+    public Vector3 TPP_CameraOffset = new Vector3(0f, 1f, -1.25f);
+    public float TPP_CameraHeightOffset = 0.5f;
+    public Vector2 pitchMinMax = new Vector2(-40, 85);
+    public float rotationSmoothTime = 10f;
+    private Vector3 currentRotation;
+    private float yaw;
+    private float pitch;
+    private Transform _fpsCameraHelper;
+    private Transform _tppCameraHelper;
+
+    [Header("Speeds")]
+    public float moveSpeed = 5f;
+    public float returnSpeed = 10f;
+    public float wallPush = 0.5f;
+
+    [Header("Distances")]
+    public float closestDistanceToPlayer = 2f;
+    public float evenCloserDistanceToPlayer = 1f;
+
+    [Header("Mask")]
+    public LayerMask collisionMask;
+
+    private bool pitchLock = false;
+
+
+
+
+    private void Start()
+    {
+        if (lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        if (CharacterAnimator)
+        {
+            Add_FPSCamPositionHelper();
+            Add_TPPCamPositionHelper();
+        }
+    }
+    private void Add_FPSCamPositionHelper()
+    {
+        _fpsCameraHelper = new GameObject().transform;
+        _fpsCameraHelper.name = "_fpsCameraHelper";
+        _fpsCameraHelper.SetParent(CharacterAnimator.GetBoneTransform(HumanBodyBones.Head));
+        _fpsCameraHelper.localPosition = Vector3.zero;
+    }
+    private void Add_TPPCamPositionHelper()
+    {
+        _tppCameraHelper = new GameObject().transform;
+        _tppCameraHelper.name = "_tppCameraHelper";
+        _tppCameraHelper.SetParent(CharacterControllerTransform);
+        _tppCameraHelper.localPosition = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            SwitchCameraPerspective();
+        }
+    }
+    public void SwitchCameraPerspective()
+    {
+        if (cameraPerspective == CameraPerspective.FirstPerson)
+        {
+            cameraPerspective = CameraPerspective.ThirdPerson;
+            SetCameraHelperPosition_TPP();
+        }
+        else
+        {
+            cameraPerspective = CameraPerspective.FirstPerson;
+            SetCameraHelperPosition_FPS();
+        }
+    }
+    private void SetCameraHelperPosition_FPS()
+    {
+        if (CharacterAnimator)
+        {
+            _fpsCameraHelper.localPosition = FPS_CameraOffset;
+            transform.position = _fpsCameraHelper.position;
+        }
+    }
+    private void SetCameraHelperPosition_TPP()
+    {
+        if (CharacterAnimator)
+        {
+            _tppCameraHelper.localPosition = TPP_CameraOffset;
+            transform.position = _tppCameraHelper.position;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        CameraControl();
+    }
+
+    private void CameraControl()
+    {
+        if (cameraPerspective == CameraPerspective.ThirdPerson)
+        {
+            CollisionCheck(CharacterControllerTransform.position - (Vector3.down * TPP_CameraHeightOffset) - (transform.forward * closestDistanceToPlayer));
+            WallCheck();
+        }
+        else
+        {
+            pitchLock = false;
+        }
+
+        if (!pitchLock)
+        {
+            yaw += Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+            pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+            currentRotation = Vector3.Lerp(currentRotation, new Vector3(pitch, yaw), rotationSmoothTime * Time.deltaTime);
+        }
+        else
+        {
+            yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+            pitch = pitchMinMax.y;
+            currentRotation = Vector3.Lerp(currentRotation, new Vector3(pitch, yaw), rotationSmoothTime * Time.deltaTime);
+        }
+
+        transform.eulerAngles = currentRotation;
+        Vector3 e = transform.eulerAngles;
+        e.x = 0;
+        CharacterControllerTransform.eulerAngles = e;
+    }
+
+    private void WallCheck()
+    {
+        Ray ray = new Ray(CharacterControllerTransform.position, -CharacterControllerTransform.forward);
+        RaycastHit hit;
+
+        if (Physics.SphereCast(ray, 0.1f, out hit, 0.5f, collisionMask))
+        {
+            pitchLock = true;
+        }
+        else
+        {
+            pitchLock = false;
+        }
+    }
+
+    private void CollisionCheck(Vector3 retPoint)
+    {
+        RaycastHit hit;
+
+        if (Physics.Linecast(CharacterControllerTransform.position, retPoint, out hit, collisionMask))
+        {
+            Vector3 norm = hit.normal * wallPush;
+            Vector3 p = hit.point + norm;
+            if (!(Vector3.Distance(Vector3.Lerp(transform.position, p, moveSpeed * Time.deltaTime), CharacterControllerTransform.position) <= evenCloserDistanceToPlayer))
+            {
+                transform.position = Vector3.Lerp(transform.position, p, moveSpeed * Time.deltaTime);
+            }
+            return;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, retPoint, returnSpeed * Time.deltaTime);
+        pitchLock = false;
+    }
+}
---
+++ Assets/MMO Game/_Script/PlayerMovementController.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerStates
+{
+    Idle,
+    Walking,
+    Running,
+    Jumping
+}
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public PlayerStates playerStates;
+    private CharacterController characterController;
+    private AudioSource _audioSource;
+
+    [Header("Player Motor")]
+    [Range(1f, 15f)]
+    public float walkSpeed = 2.5f;
+    [Range(1f, 15f)]
+    public float runSpeed = 6f;
+    [Range(1f, 15f)]
+    public float JumpForce = 6f;
+
+    [Header("Animator and Parameters")]
+    public Animator CharacterAnimator;
+    private float HorzAnimation;
+    private float VertAnimation;
+    private bool JumpAnimation;
+    private bool LandAnimation;
+    private float _footstepDelay;
+    private float footstep_et;
+
+    [Header("Sounds")]
+    public List<AudioClip> FootstepSounds;
+    public List<AudioClip> JumpSounds;
+    public List<AudioClip> LandSounds;
+
+
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        _audioSource = gameObject.AddComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        //handle controller
+        HandlePlayerControls();
+
+        //sync animations with controller
+        SetCharacterAnimations();
+
+        //sync footsteps with controller
+        PlayFootstepSounds();
+    }
+
+    void HandlePlayerControls()
+    {
+        float hInput = Input.GetAxisRaw("Horizontal");
+        float vInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 fwdMovement = characterController.isGrounded == true ? transform.forward * vInput : Vector3.zero;
+        Vector3 rightMovement = characterController.isGrounded == true ? transform.right * hInput : Vector3.zero;
+
+        float _speed = vInput > 0.8f ? runSpeed : walkSpeed;
+        characterController.SimpleMove(Vector3.ClampMagnitude(fwdMovement + rightMovement, 1f) * _speed);
+
+        if (characterController.isGrounded)
+        {
+            Jump();
+        }
+
+        //Managing Player States
+        if (characterController.isGrounded)
+        {
+            if (hInput == 0 && vInput == 0)
+            {
+                Debug.Log("Idle Player");
+                playerStates = PlayerStates.Idle;
+            }
+            else
+            {
+                if (_speed == walkSpeed)
+                    playerStates = PlayerStates.Walking;
+                else
+                    playerStates = PlayerStates.Running;
+
+                _footstepDelay = (2 / _speed);
+            }
+        }
+        else
+        {
+            playerStates = PlayerStates.Jumping;
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(PerformJumpRoutine());
+            JumpAnimation = true;
+        }
+    }
+
+    IEnumerator PerformJumpRoutine()
+    {
+        //play jump sound
+        if (_audioSource)
+            _audioSource.PlayOneShot(JumpSounds[Random.Range(0, JumpSounds.Count)]);
+
+        float _jump = JumpForce;
+
+        do
+        {
+            characterController.Move(Vector3.up * _jump * Time.deltaTime);
+            _jump -= Time.deltaTime;
+            yield return null;
+        }
+        while (!characterController.isGrounded);
+
+        //play land sound
+        if (_audioSource)
+        {
+            _audioSource.PlayOneShot(LandSounds[Random.Range(0, LandSounds.Count)]);
+        }
+    }
+
+    void SetCharacterAnimations()
+    {
+        if (!CharacterAnimator)
+            return;
+
+        switch (playerStates)
+        {
+            case PlayerStates.Idle:
+                HorzAnimation = Mathf.Lerp(HorzAnimation, 0, 5 * Time.deltaTime);
+                VertAnimation = Mathf.Lerp(VertAnimation, 0, 5 * Time.deltaTime);
+                break;
+
+            case PlayerStates.Walking:
+                HorzAnimation = Mathf.Lerp(HorzAnimation, 1 * Input.GetAxis("Horizontal"), 5 * Time.deltaTime);
+                VertAnimation = Mathf.Lerp(VertAnimation, 1 * Input.GetAxis("Vertical"), 5 * Time.deltaTime);
+                break;
+
+            case PlayerStates.Running:
+                HorzAnimation = Mathf.Lerp(HorzAnimation, 2 * Input.GetAxis("Horizontal"), 5 * Time.deltaTime);
+                VertAnimation = Mathf.Lerp(VertAnimation, 2 * Input.GetAxis("Vertical"), 5 * Time.deltaTime);
+                break;
+
+            case PlayerStates.Jumping:
+                if (JumpAnimation)
+                {
+                    CharacterAnimator.SetTrigger("Jump");
+                    JumpAnimation = false;
+                }
+                break;
+        }
+
+        LandAnimation = characterController.isGrounded;
+        CharacterAnimator.SetFloat("Horizontal", HorzAnimation);
+        CharacterAnimator.SetFloat("Vertical", VertAnimation);
+        CharacterAnimator.SetBool("isGrounded", LandAnimation);
+    }
+
+    void PlayFootstepSounds()
+    {
+        if (playerStates == PlayerStates.Idle || playerStates == PlayerStates.Jumping)
+            return;
+
+        if (footstep_et < _footstepDelay)
+        {
+            footstep_et += Time.deltaTime;
+        }
+        else
+        {
+            footstep_et = 0;
+            _audioSource.PlayOneShot(FootstepSounds[Random.Range(0, FootstepSounds.Count)]);
+        }
+    }
+}
